117b31729ea432e1e51c131133ffd51a012ea8e4
blsgamesy-hub <bls.game.sy@gmail.com>
2026-03-19 19:35:28 +0900

    Add files via upload

---
+++ sw_journal.js
@@ -0,0 +1,30 @@
+const CACHE = 'journal-v1';
+const FILES = [
+  './journal.html',
+  './journal_icon.png',
+  './manifest_journal.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
